962550da83ae75f9913abdf01befdec487ab7b02
Aziz97-MSR <msr.aziz97@gmail.com>
2026-04-16 11:52:57 +0600

    all get

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ async function run() {
     const awardcollection = await client.db("aura").collection('awards')
     const educationcollection = await client.db("aura").collection('education')
     const skillcollection = await client.db("aura").collection('skills')
+    const herocollection = await client.db("aura").collection('home')
+    const projectcollection = await client.db("aura").collection('project')
+    const navcollection = await client.db("aura").collection('nav')
+    const footercollection = await client.db("aura").collection('footer')
+    const welcomecollection = await client.db("aura").collection('welcome')
 
     app.get('/about', async(req, res)=>
     {
@@ -71,6 +76,46 @@ async function run() {
         const awardData = await cursour.toArray();
         res.send(awardData)
     })
+    app.get('/hero', async(req, res)=>
+    {
+        const query = {}
+        const cursour = herocollection.find(query)
+        const herodData = await cursour.toArray();
+        res.send(heroData)
+    }
+)
+    app.get('/award', async(req, res)=>
+    {
+        const query = {}
+        const cursour = projectcollection.find(query)
+        const projectData = await cursour.toArray();
+        res.send(projectData)
+    })
+
+    app.get('/nav', async(req, res)=>
+    {
+        const query = {}
+        const cursour = navcollection.find(query)
+        const navData = await cursour.toArray();
+        res.send(navData)
+    })
+
+    app.get('/footer', async(req, res)=>
+    {
+        const query = {}
+        const cursour = footercollection.find(query)
+        const footerData = await cursour.toArray();
+        res.send(footerData)
+    })
+    app.get('/intro', async(req, res)=>
+    {
+        const query = {}
+        const cursour = welcomecollection.find(query)
+        const introData = await cursour.toArray();
+        res.send(introData)
+    })
+
+    
 
 
 
