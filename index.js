71fc547df8d76c67ec3919fd7e5cd2416632cc86
Aziz97-MSR <msr.aziz97@gmail.com>
2026-04-21 18:22:36 +0600

    add new items

--- index.js
+++ index.js
@@ -14,122 +14,214 @@ app.use(express.json())
 const uri = `mongodb+srv://msraziz97_db_user:l08VXQsxPi1dGCPH@authority-ledger.vworqap.mongodb.net/?appName=Authority-Ledger`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 async function run() {
-  try {
-    await client.connect();
+    try {
+        await client.connect();
 
-    
-    const aboutcollection = await client.db("aura").collection('about')
-    const experiencecollection = await client.db("aura").collection('experience')
-    const awardcollection = await client.db("aura").collection('awards')
-    const educationcollection = await client.db("aura").collection('education')
-    const skillcollection = await client.db("aura").collection('skills')
-    const herocollection = await client.db("aura").collection('home')
-    const projectcollection = await client.db("aura").collection('projects')
-    const navcollection = await client.db("aura").collection('nav')
-    const footercollection = await client.db("aura").collection('footer')
-    const welcomecollection = await client.db("aura").collection('welcome')
 
-    app.get('/about', async(req, res)=>
-    {
-        const query = {}
-        const cursour = aboutcollection.find(query)
-        const aboutData = await cursour.toArray();
-        res.send(aboutData)
-    })
+        const aboutcollection = await client.db("aura").collection('about')
+        const experiencecollection = await client.db("aura").collection('experience')
+        const awardcollection = await client.db("aura").collection('awards')
+        const educationcollection = await client.db("aura").collection('education')
+        const skillcollection = await client.db("aura").collection('skills')
+        const herocollection = await client.db("aura").collection('home')
+        const projectcollection = await client.db("aura").collection('projects')
+        const navcollection = await client.db("aura").collection('nav')
+        const footercollection = await client.db("aura").collection('footer')
+        const welcomecollection = await client.db("aura").collection('welcome')
 
-    app.get('/education', async(req, res)=>
-    {
-        const query = {}
-        const cursour = educationcollection.find(query)
-        const educationData = await cursour.toArray();
-        res.send(educationData)
-    })
+        app.get('/about', async (req, res) => {
+            const query = {}
+            const cursour = aboutcollection.find(query)
+            const aboutData = await cursour.toArray();
+            res.send(aboutData)
+        })
 
-    app.get('/skills', async(req, res)=>
-    {
-        const query = {}
-        const cursour = skillcollection.find(query)
-        const skillData = await cursour.toArray();
-        res.send(skillData)
-    })
+        app.get('/education', async (req, res) => {
+            const query = {}
+            const cursour = educationcollection.find(query)
+            const educationData = await cursour.toArray();
+            res.send(educationData)
+        })
 
-    
-    app.get('/experience', async(req, res)=>
-    {
-        const query = {}
-        const cursour = experiencecollection.find(query)
-        const experienceData = await cursour.toArray();
-        res.send(experienceData)
-    })
+        app.get('/skills', async (req, res) => {
+            const query = {}
+            const cursour = skillcollection.find(query)
+            const skillData = await cursour.toArray();
+            res.send(skillData)
+        })
 
-    app.get('/award', async(req, res)=>
-    {
-        const query = {}
-        const cursour = awardcollection.find(query)
-        const awardData = await cursour.toArray();
-        res.send(awardData)
-    })
-    app.get('/hero', async(req, res)=>
-    {
-        const query = {}
-        const cursour = herocollection.find(query)
-        const heroData = await cursour.toArray();
-        res.send(heroData)
+
+        app.get('/experience', async (req, res) => {
+            const query = {}
+            const cursour = experiencecollection.find(query)
+            const experienceData = await cursour.toArray();
+            res.send(experienceData)
+        })
+
+        app.get('/award', async (req, res) => {
+            const query = {}
+            const cursour = awardcollection.find(query)
+            const awardData = await cursour.toArray();
+            res.send(awardData)
+        })
+        app.get('/hero', async (req, res) => {
+            const query = {}
+            const cursour = herocollection.find(query)
+            const heroData = await cursour.toArray();
+            res.send(heroData)
+        }
+        )
+        app.get('/project', async (req, res) => {
+            const query = {}
+            const cursour = projectcollection.find(query)
+            const projectData = await cursour.toArray();
+            res.send(projectData)
+        })
+
+        app.get('/nav', async (req, res) => {
+            const query = {}
+            const cursour = navcollection.find(query)
+            const navData = await cursour.toArray();
+            res.send(navData)
+        })
+
+        app.get('/footer', async (req, res) => {
+            const query = {}
+            const cursour = footercollection.find(query)
+            const footerData = await cursour.toArray();
+            res.send(footerData)
+        })
+        app.get('/intro', async (req, res) => {
+            const query = {}
+            const cursour = welcomecollection.find(query)
+            const introData = await cursour.toArray();
+            res.send(introData)
+        })
+
+        app.put('/about/:id', async (req, res) => {
+            const id = req.params.id
+            const updatedItem = req.body
+
+            const options = { upsert: true };
+            const filter = { _id: new ObjectId(id) };
+
+            const updatedDoc = {
+                $set: {
+                    paragraph: updatedItem.paragraph
+                }
+            }
+            const result = await aboutcollection.updateOne(filter, updatedDoc, options);
+
+            res.send(result);
+        })
+        app.put('/footer/:id', async (req, res) => {
+            const id = req.params.id
+            const updatedItem = req.body
+            const options = { upsert: true };
+             const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    "grid1.paragraph": updatedItem.paragraph
+                }
+            }
+            const result = await footercollection.updateOne(filter, updatedDoc, options);
+
+            res.send(result);
+        })
+
+
+        app.put('/experience/:id', async (req, res) => {
+            const id = req.params.id
+            const newItem = req.body
+
+            const result = await experiencecollection.updateOne(
+                { _id: new ObjectId(id) },
+                {
+                    $push: {
+                        experience: newItem
+                    }
+                }
+            );
+            res.send(result)
+        })
+        app.put('/education/:id', async (req, res) => {
+            const id = req.params.id
+            const newItem = req.body
+
+            const result = await educationcollection.updateOne(
+                { _id: new ObjectId(id) },
+                {
+                    $push: {
+                        items: newItem
+                    }
+                }
+            );
+            res.send(result)
+        })
+        app.put('/skill/:id', async (req, res) => {
+            const id = req.params.id
+            const newItem = req.body
+
+            const result = await skillcollection.updateOne(
+                { _id: new ObjectId(id) },
+                {
+                    $push: {
+                        items: newItem
+                    }
+                }
+            );
+            res.send(result)
+        })
+        app.put('/feature/:id', async (req, res) => {
+            const id = req.params.id
+            const newItem = req.body
+
+            const result = await projectcollection.updateOne(
+                { _id: new ObjectId(id) },
+                {
+                    $push: {
+                        items: newItem
+                    }
+                }
+            );
+            res.send(result)
+        })
+        app.put('/award/:id', async (req, res) => {
+            const id = req.params.id
+            const newItem = req.body
+
+            const result = await awardcollection.updateOne(
+                { _id: new ObjectId(id) },
+                {
+                    $push: {
+                        items: newItem
+                    }
+                }
+            );
+            res.send(result)
+        })
+
+
+
+
+
+
+
     }
-)
-    app.get('/project', async(req, res)=>
-    {
-        const query = {}
-        const cursour = projectcollection.find(query)
-        const projectData = await cursour.toArray();
-        res.send(projectData)
-    })
-
-    app.get('/nav', async(req, res)=>
-    {
-        const query = {}
-        const cursour = navcollection.find(query)
-        const navData = await cursour.toArray();
-        res.send(navData)
-    })
-
-    app.get('/footer', async(req, res)=>
-    {
-        const query = {}
-        const cursour = footercollection.find(query)
-        const footerData = await cursour.toArray();
-        res.send(footerData)
-    })
-    app.get('/intro', async(req, res)=>
-    {
-        const query = {}
-        const cursour = welcomecollection.find(query)
-        const introData = await cursour.toArray();
-        res.send(introData)
-    })
-
-    
-
-
-
-
-
-    } 
-   finally {
+    finally {
         /* await client.close(); */
     }
 }
 run().catch(console.dir);
 
-app.get('/' , (req , res)=>
-{
+app.get('/', (req, res) => {
     res.send("Aura server is running")
 })
 
